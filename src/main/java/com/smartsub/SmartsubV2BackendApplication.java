b3730414a4723b8a1ac848aeebf16ad6baa85069
kjp0411 <98109773+kjp0411@users.noreply.github.com>
2026-06-06 15:22:54 +0900

    chore: initialize SmartSub V2 backend project

---
+++ src/main/java/com/smartsub/SmartsubV2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartsubV2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartsubV2BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartsub/SmartsubV2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartsubV2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
